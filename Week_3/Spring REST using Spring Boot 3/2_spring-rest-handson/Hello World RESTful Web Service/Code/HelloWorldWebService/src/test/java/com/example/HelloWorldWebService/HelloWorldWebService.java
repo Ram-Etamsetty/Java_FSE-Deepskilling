fe7907ff59750f3457eb3e8d429e66699e726c86
Ram-Etamsetty <etamsettyram2005@gmail.com>
2026-07-06 14:46:30 +0530

    Created a HelloWorld Restful web service

---
+++ Week_3/Spring REST using Spring Boot 3/2_spring-rest-handson/Hello World RESTful Web Service/Code/HelloWorldWebService/src/test/java/com/example/HelloWorldWebService/HelloWorldWebService.java
@@ -0,0 +1,11 @@
+package com.example.HelloWorldWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWebService {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldWebService.class, args);
+    }
+}
