f584b0810c4c87be63ccc3a12fac7b03535cc915
Ram-Etamsettygit config --global user.email <etamsettyram2005@gmail.com>
2026-07-18 08:40:39 +0530

    Completed ReactS-HOL-2 task

---
+++ Week_5/2. ReactJS-HOL.docx/Code/studentapp/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Welcome to the About page of Student Management Portal</h3>
+      </div>
+    );
+  }
+}
+
+export default About;
