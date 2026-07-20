c607b881f35d0ce447b2eb3335ffe1e22bfb8b41
Ram-Etamsetty <etamsettyram2005@gmail.com>
2026-07-20 15:00:17 +0530

    Created a Blogger App as mentioned in 13. ReactHOL

--- Week_5/12. ReactJS-HOL/ticketbookingapp/src/GuestGreeting.js
+++ Week_5/12. ReactJS-HOL/ticketbookingapp/src/GuestGreeting.js
@@ -1,34 +1,7 @@
 function GuestGreeting() {
   return (
     <div>
-      <h1>Flight Details</h1>
-
-      <table cellPadding="5">
-        <thead>
-          <tr>
-            <th>Flight</th>
-            <th>Source</th>
-            <th>Destination</th>
-            <th>Fare</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>AI101</td>
-            <td>Hyderabad</td>
-            <td>Delhi</td>
-            <td>₹5000</td>
-          </tr>
-
-          <tr>
-            <td>6E202</td>
-            <td>Chennai</td>
-            <td>Mumbai</td>
-            <td>₹4500</td>
-          </tr>
-        </tbody>
-      </table>
+      
 
       <p><b>Please Login to Book Tickets.</b></p>
     </div>
